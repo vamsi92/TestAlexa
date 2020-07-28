2e4117f6138fbd2134c8abbd5874b69ff85cda7c
vamyarla <vamyarla@VAMYARLA-IN.oradev.oraclecorp.com>
2020-07-28 11:30:37 +0530

    addng all

---
+++ src/main/java/com/commercecloud/getOrderService/GetOrderServiceApplication.java
@@ -0,0 +1,10 @@
+package com.commercecloud.getOrderService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetOrderServiceApplication {
+
+
+}
