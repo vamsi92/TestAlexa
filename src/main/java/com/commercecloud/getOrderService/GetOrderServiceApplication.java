25727b7c70fd2d87fc003fa53232a4381cae0c79
vamyarla <vamyarla@VAMYARLA-IN.oradev.oraclecorp.com>
2020-07-28 11:44:02 +0530

    mod

--- src/main/java/com/commercecloud/getOrderService/GetOrderServiceApplication.java
+++ src/main/java/com/commercecloud/getOrderService/GetOrderServiceApplication.java
@@ -17,46 +17,22 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class GetOrderServiceApplication {
     String url="http://cc-demo-3.ad3.fusionappsdephx.oraclevcn.com:9080/ccadminui/v1";
-    @GetMapping("/")
+
+@GetMapping("/")
 public String sayHi(){
 
     return "Hello Spring";
 }
 
-@GetMapping("/{orderId}")
-public String getOrderDetails(@PathVariable("orderId") String orderId){
+    @GetMapping("/message/{input}")
+    public String getMessage(@PathVariable String input){
 
-    String accessToken=userLogin("admin","admin","123456");
-    RestTemplate restTemplate=new RestTemplate();
-    HttpHeaders headers =new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_JSON);
-    headers.set("Authorization","Bearer "+accessToken);
-    HttpEntity entity=new HttpEntity(headers);
-    ResponseEntity<String> response=restTemplate.exchange(url+"/orders/"+orderId, HttpMethod.GET,entity,String.class);
-    JSONObject orderDetailsResponse=new JSONObject(response.getBody());
-    JSONObject jsObj=(JSONObject) ((JSONArray) orderDetailsResponse.get("commerceItems")).get(0);
-    return jsObj.get("productDisplayName").toString();
-
-}
-
-    public String userLogin(String email, String password,String otp) {
-        String url = new StringBuilder()
-                .append("http://cc-demo-3.ad3.fusionappsdephx.oraclevcn.com:9080/ccadminui/v1/mfalogin")
-                .toString();
-
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        MultiValueMap<String, String> map= new LinkedMultiValueMap<String, String>();
-        map.add("grant_type", "password");
-        map.add("username", email);
-        map.add("password", password);
-        map.add("totp_code",otp);
-
-        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
-
-        ResponseEntity<String> response = restTemplate.postForEntity( url, request , String.class);
-        JSONObject responseObj=new JSONObject(response.getBody());
-        return responseObj.get("access_token").toString();
+        return url+ input;
     }
+
+
+public static void main(String args[]){
+
+        SpringApplication.run(GetOrderServiceApplication.class,args);
+}
 }
